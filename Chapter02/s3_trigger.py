6d00551fbfef0376171d6fa803e0c396c3d5fc7c
Krishna <ksankar42@gmail.com>
2020-02-25 09:18:18 -0800

    Renamed directories and refreshed contents

--- Chapter02/s3_trigger.py
+++ Chapter02/s3_trigger.py
@@ -9,7 +9,8 @@ def lambda_handler(event, context):
         # enter the specific bucket name to the 'bucket' variable
         bucket = "<input bucket name>"
         
-        # event – AWS Lambda uses this parameter to pass in event data to the handler. This parameter is usually of the Python dict type. It can also be list, str, int, float, or NoneType type
+        # event – AWS Lambda uses this parameter to pass in event data to the handler. 
+        # This parameter is usually of the Python dict type. It can also be list, str, int, float, or NoneType type
         text_file_obj = event["Records"][0]
         # assign the uploaded text file name to the 'filename' variable 
         filename = str(text_file_obj['s3']['object']['key'])
@@ -25,7 +26,7 @@ def lambda_handler(event, context):
         body_str_obj = str(file_obj['Body'].read())
         
         # create a comprehend object
-        comprehend = boto3.client(service_name="comprehend", region_name='<input region>') #us-east-1
+        comprehend = boto3.client(service_name="comprehend")
         
         # call detect_sentiment()
         sentiment_response = comprehend.detect_sentiment(Text = body_str_obj, LanguageCode = "en")
@@ -39,4 +40,7 @@ def lambda_handler(event, context):
         key_phases_response = comprehend.detect_key_phrases(Text = body_str_obj, LanguageCode = "en") 
         print("\n\nkey_phases_response: \n", key_phases_response)
       
-        return 'Hello from Lambda'
+        return return {
+            'statusCode' :200,
+            'body' : jason.dumps('Hello from Lambda')
+        }
